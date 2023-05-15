2130798c2ec2834370ad26961c2fe78892c19e51
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-15 19:58:07 +0100

    feat: moved snake widget into widgets layer

---
+++ lib/screen/widgets/snake_pixel.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SnakePixel extends StatelessWidget {
+  const SnakePixel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+    );
+  }
+}
