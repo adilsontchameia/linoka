aecaab868db4d7da9e0dd1c224594fa60ab6f841
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-15 19:48:06 +0100

    feat: divied the home page to another file

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:snake_game/screen/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
