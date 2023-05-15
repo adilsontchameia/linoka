db72fa68930322b1f3be7060775e2f4f3abd27ae
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-15 20:01:44 +0100

    feat: food widget for snake

---
+++ lib/screen/widgets/food_pixel.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FoodPixel extends StatelessWidget {
+  const FoodPixel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.green,
+          borderRadius: BorderRadius.circular(4),
+        ),
+      ),
+    );
+  }
+}
