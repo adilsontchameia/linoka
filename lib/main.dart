da2ae34fbc2d6787ce76ebeb8b92dc9842b09c83
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-16 10:21:38 +0100

    feat: config for device preview and app root

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:snake_game/providers/snake_commands_provider.dart';
 import 'package:snake_game/screen/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const MyApp(), // Wrap your app
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
-      home: const HomePage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => SnakeCommandsProvider(), lazy: false),
+      ],
+      child: MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+        home: const HomePage(),
+      ),
     );
   }
 }
