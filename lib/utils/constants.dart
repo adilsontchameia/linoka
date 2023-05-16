c165d8985385ebe4c37203ead36fd2d493e7bd67
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-16 10:23:15 +0100

    feat: app constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class AppConstants {
+  static const backgroundContainerGridColor = Color.fromARGB(255, 22, 22, 22);
+  static const backgroundContainerColor = Color.fromARGB(255, 22, 22, 22);
+  //Audio Section Constants
+  static const pause = 'Pause';
+  static const score = 'Score';
+  static const play = 'Play';
+  static const backgroundSoundPath = 'fluffing_a_duck.mp3';
+  static const snakeEatenSoundPath = 'torch_whoosh_panned.mp3';
+  //Settings Sections Constants
+  static const logImgPath = 'assets/logo.png';
+  static const volumeSettings = 'Volume Settings';
+  static const appInfo = 'This App Was Developed By Adilson Tchameia.';
+  static const emailInfo = 'Email: adilsontchameia@gmail.com';
+  static const socialInfo = 'Linkedin/Github: Adilson Tchameia';
+  static const uiInfo = 'UI: Adilson Tchameia';
+  static const audioInfo = 'Audio: Yago Tchameia';
+  static const yearInfo = '@2023';
+}
