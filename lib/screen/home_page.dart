72a51f490a5b318ba17af0ef070dc226260c4c8e
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-16 10:22:24 +0100

    feat: showing the grid view reponsively

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:snake_game/providers/snake_commands_provider.dart';
+import 'package:snake_game/screen/widgets/app_info_alert.dart';
 import 'package:snake_game/screen/widgets/blank_pixel.dart';
+import 'package:snake_game/screen/widgets/custom_elevated_button.dart';
+import 'package:snake_game/screen/widgets/score_widget.dart';
 import 'package:snake_game/screen/widgets/snake_pixel.dart';
 import '../utils/constants.dart';
-import '../utils/snack_direction_logic.dart';
+import '../utils/snake_direction_logic.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -47,80 +50,32 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     final gameProvider = context.watch<SnakeCommandsProvider>();
     return Scaffold(
       backgroundColor: Colors.black12,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-/*
-            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-              Expanded(
-                  flex: 0,
-                  child: Row(children: [
-                    ScoreWidget(gameProvider: gameProvider),
-                    AppInfoAlertDialog(
-                        backgroundSound: gameProvider.backgroundSound,
-                        backgroundVolume: gameProvider.backgroundVolume)
-                  ])),
-              Expanded(
-                flex: 0,
-                child: Container(
-                  color: AppConstants.backgroundContainerColor,
-                  child: Expanded(
-                    child: Row(
-                      children: [
-                        CustomElevatedButton(
-                            gameProvider: gameProvider,
-                            foregroundColor: Colors.white,
-                            backgroundColor1: Colors.green,
-                            buttonTitle: AppConstants.play,
-                            onPressed: () {
-                              playButtonLogic(gameProvider);
-                            }),
-                        const SizedBox(width: 5.0),
-                        AbsorbPointer(
-                          absorbing:
-                              !gameProvider.gamehasStarted ? true : false,
-                          child: CustomElevatedButton(
-                              gameProvider: gameProvider,
-                              foregroundColor: Colors.white,
-                              backgroundColor1: Colors.grey,
-                              backgroundColor2: Colors.amber,
-                              buttonTitle: AppConstants.pause,
-                              onPressed: () {
-                                gameProvider.pauseGame();
-                                gameProvider.gamehasPaused = true;
-                              }),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-
-            ]),
-            */
-            //GameGrid
-            Expanded(
-              flex: 4,
-              child: GestureDetector(
-                onVerticalDragUpdate: (details) {
-                  //If Delta Y is Positive
-                  swipeDownAndUp(details, gameProvider);
-                },
-                onHorizontalDragUpdate: (details) {
-                  //If Delta Y is Positive
-                  swipeLeftAndRight(details, gameProvider);
-                },
-                child: Container(
-                  color: AppConstants.backgroundContainerGridColor,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5.5),
-                    child: GridView.builder(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                //If Delta Y is Positive
+                swipeDownAndUp(details, gameProvider);
+              },
+              onHorizontalDragUpdate: (details) {
+                //If Delta Y is Positive
+                swipeLeftAndRight(details, gameProvider);
+              },
+              child: Container(
+                color: AppConstants.backgroundContainerGridColor,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 5.5),
+                  child: LayoutBuilder(
+                    builder: (context, constraints) => GridView.builder(
                       itemCount: gameProvider.totalNumberSquares,
+                      shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: gameProvider.rowSize,
+                        childAspectRatio:
+                            constraints.biggest.aspectRatio * 3 / 2,
                       ),
                       itemBuilder: (context, index) {
                         if (gameProvider.snakePosition.contains(index)) {
@@ -140,9 +95,43 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
                 ),
               ),
             ),
-            //Game Config Board
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            color: AppConstants.backgroundContainerColor,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomElevatedButton(
+                    gameProvider: gameProvider,
+                    foregroundColor: Colors.white,
+                    backgroundColor1: Colors.green,
+                    buttonTitle: AppConstants.play,
+                    onPressed: () {
+                      playButtonLogic(gameProvider);
+                    }),
+                const SizedBox(width: 5.0),
+                AbsorbPointer(
+                  absorbing: !gameProvider.gamehasStarted ? true : false,
+                  child: CustomElevatedButton(
+                      gameProvider: gameProvider,
+                      foregroundColor: Colors.white,
+                      backgroundColor1: Colors.grey,
+                      backgroundColor2: Colors.amber,
+                      buttonTitle: AppConstants.pause,
+                      onPressed: () {
+                        gameProvider.pauseGame();
+                        gameProvider.gamehasPaused = true;
+                      }),
+                ),
+                ScoreWidget(gameProvider: gameProvider),
+                AppInfoAlertDialog(
+                    backgroundSound: gameProvider.backgroundSound,
+                    backgroundVolume: gameProvider.backgroundVolume),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
