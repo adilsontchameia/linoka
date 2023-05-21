ef016b663934a8c9ffdd3e3f92244ce93e21fdc6
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-21 14:56:43 +0100

    fix: importing enums to guide the snake

--- lib/utils/snake_direction_logic.dart
+++ lib/utils/snake_direction_logic.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../providers/snake_commands_provider.dart';
-import 'game_command_enums.dart';
 
 void swipeLeftAndRight(
     DragUpdateDetails details, SnakeCommandsProvider gameProvider) {
   if (details.delta.dx > 0 &&
-      gameProvider.currentDirection != SnakeDirection.LEFT) {
+      gameProvider.currentDirection != SnakeDirection.left) {
     //Moving Right
-    gameProvider.currentDirection = SnakeDirection.RIGHT;
+    gameProvider.currentDirection = SnakeDirection.right;
   } else if (details.delta.dx < 0 &&
-      gameProvider.currentDirection != SnakeDirection.RIGHT) {
+      gameProvider.currentDirection != SnakeDirection.right) {
     //Moving Left
-    gameProvider.currentDirection = SnakeDirection.LEFT;
+    gameProvider.currentDirection = SnakeDirection.left;
   }
 }
 
 void swipeDownAndUp(
     DragUpdateDetails details, SnakeCommandsProvider gameProvider) {
   if (details.delta.dy > 0 &&
-      gameProvider.currentDirection != SnakeDirection.UP) {
+      gameProvider.currentDirection != SnakeDirection.up) {
     //Moving Down
-    gameProvider.currentDirection = SnakeDirection.DOWN;
+    gameProvider.currentDirection = SnakeDirection.down;
   } else if (details.delta.dy < 0 &&
-      gameProvider.currentDirection != SnakeDirection.DOWN) {
+      gameProvider.currentDirection != SnakeDirection.down) {
     //Moving Up
-    gameProvider.currentDirection = SnakeDirection.UP;
+    gameProvider.currentDirection = SnakeDirection.up;
   }
 }
