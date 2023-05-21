6dc0b94f3bb8dc9d8947a061a5a1b0ffe4de0d7f
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-05-21 11:42:04 +0100

    feat: animated snake

--- lib/widgets/food_pixel.dart
+++ lib/widgets/food_pixel.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
-class FoodPixel extends StatelessWidget {
-  const FoodPixel({super.key});
+class FoodBlinkingWidget extends StatefulWidget {
+  const FoodBlinkingWidget({super.key, required this.typeFood});
+  final String typeFood;
+  @override
+  FoodBlinkingWidgetState createState() => FoodBlinkingWidgetState();
+}
+
+class FoodBlinkingWidgetState extends State<FoodBlinkingWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    _animationController!.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.green,
-          borderRadius: BorderRadius.circular(4),
-        ),
+    return FadeTransition(
+      opacity: _animationController!,
+      child: const Text(
+        ' widget.typeFood',
+        style: TextStyle(fontSize: 18.0),
       ),
     );
   }
